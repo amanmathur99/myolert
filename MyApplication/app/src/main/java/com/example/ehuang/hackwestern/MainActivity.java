c81a376b12959255732b301b8df7e29a7785a74b
Nicholas Vadivelu <nick@bassoft.com>
2017-11-18 06:00:49 -0500

    added dependencies for myo

--- MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
@@ -7,17 +7,34 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.thalmic.myo.*;
+import com.thalmic.myo.scanner.ScanActivity;
+
 import android.view.View.OnClickListener;
 
 
 public class MainActivity extends AppCompatActivity {
+    //Need this because TAG is private otherwise
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize the hub to be used by the myo device
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            Log.e(TAG, "Could not initialize the Hub.");
+            finish();
+            return;
+        }
+
+        // Will connect with a myo device that is very close to phone
+        Hub.getInstance().attachToAdjacentMyo();
     }
 
     private Button b;
