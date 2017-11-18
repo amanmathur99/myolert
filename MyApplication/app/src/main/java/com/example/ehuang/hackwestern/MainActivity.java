a951ad0af15a2b9b7f3cd950abee7ef984c43491
amanmathur99 <amanmathur99@gmail.com>
2017-11-18 06:13:28 -0500

    phone calls

--- MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
@@ -14,28 +14,43 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private Button b;
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button);
+
+        // add button listener
+        b.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                System.out.println("we suck");
+
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:6478982744"));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    System.out.println("we rock");
+
+                    return;
+                }
+                startActivity(callIntent);
+
+            }
+
+        });
+
     }
 
-    private Button b;
-    String num = "1234567890";
-    public void addListenerOnButtonClick(){
-        b = (Button)findViewById(R.id.button);
-        b.setOnClickListener(
-                new View.OnClickListener(){
-                    @Override
-                    public void onClick(View view){
-                        Intent callintent = new Intent(Intent.ACTION_CALL);
-                        callintent.setData(Uri.parse("tel:"+num));
-                        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
-                            return;
-                        }
-                        startActivity(callintent);
-                    }
-                }
-        );
     }
-}
