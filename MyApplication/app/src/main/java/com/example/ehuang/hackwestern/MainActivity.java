64f6bba0e05a0f1665288a09123adc9baf5b17bc
Matthew Chaing <matthew.chiang.myc@gmail.com>
2017-11-18 08:39:35 -0500

    added text

--- MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
@@ -6,15 +6,21 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button b;
+    private EditText phoneNum , Msg;
+    private Button sendButton;
+    private int permissions_request_sendMsg = 1;
 
     public void onCreate(Bundle savedInstanceState) {
 
@@ -24,13 +30,11 @@ public class MainActivity extends AppCompatActivity {
         b = (Button) findViewById(R.id.button);
 
         // add button listener
-        b.setOnClickListener(new OnClickListener() {
+        b.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
                 System.out.println("we suck");
-
-
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:6478982744"));
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
@@ -46,11 +50,33 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
                 startActivity(callIntent);
-
             }
 
         });
+        phoneNum = (EditText) findViewById(R.id.phoneNum);
+        Msg = (EditText) findViewById(R.id.Msg);
 
+        sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = Msg.getText().toString();
+                String tele = phoneNum.getText().toString();
+                System.out.println("hellO");
+                //ask for user permission
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
+                        != PackageManager.PERMISSION_GRANTED)
+                {
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.SEND_SMS},
+                            permissions_request_sendMsg);
+                }
+                else
+                {
+                    SmsManager sms = SmsManager.getDefault();
+                    sms.sendTextMessage(tele,null,message,null,null);
+                }
+            }
+        });
     }
 
     }
