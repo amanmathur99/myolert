18b6cd8622b124341da11f63698003d6cc24cfc3
Nicholas Vadivelu <nick@bassoft.com>
2017-11-18 10:09:38 -0500

    myo armband successfully interfaced with device

--- MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.ehuang.hackwestern;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,27 +20,153 @@ import com.thalmic.myo.*;
 import com.thalmic.myo.scanner.ScanActivity;
 
 import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    //Need this because TAG is private otherwise
-    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static Activity YourActivity;
+
+    private DeviceListener mListener = new AbstractDeviceListener() {
+
+        // onConnect() is called whenever a Myo has been connected.
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to cyan when a Myo connects.
+            System.out.println("onConnect");
+        }
+
+        // onDisconnect() is called whenever a Myo has been disconnected.
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to red when a Myo disconnects.
+            System.out.println("disconnect");
+        }
+
+        // onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
+        // arm. This lets Myo know which arm it's on and which way it's facing.
+        @Override
+        public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
+            System.out.println(myo.getArm() == Arm.LEFT ? "left": "right");
+        }
+
+        // onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
+        // it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
+        // when Myo is moved around on the arm.
+        @Override
+        public void onArmUnsync(Myo myo, long timestamp) {
+            System.out.println("arm unsync");
+        }
+
+        // onUnlock() is called whenever a synced Myo has been unlocked. Under the standard locking
+        // policy, that means poses will now be delivered to the listener.
+        @Override
+        public void onUnlock(Myo myo, long timestamp) {
+            System.out.println("unlock");
+        }
+
+        // onLock() is called whenever a synced Myo has been locked. Under the standard locking
+        // policy, that means poses will no longer be delivered to the listener.
+        @Override
+        public void onLock(Myo myo, long timestamp) {
+            System.out.println("lock");
+        }
+
+        // onOrientationData() is called whenever a Myo provides its current orientation,
+        // represented as a quaternion.
+        @Override
+        public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
+            // Calculate Euler angles (roll, pitch, and yaw) from the quaternion.
+            float roll = (float) Math.toDegrees(Quaternion.roll(rotation));
+            float pitch = (float) Math.toDegrees(Quaternion.pitch(rotation));
+            float yaw = (float) Math.toDegrees(Quaternion.yaw(rotation));
+
+            // Adjust roll and pitch for the orientation of the Myo on the arm.
+            if (myo.getXDirection() == XDirection.TOWARD_ELBOW) {
+                roll *= -1;
+                pitch *= -1;
+            }
+
+            // Next, we apply a rotation to the text view using the roll, pitch, and yaw.
+            //mTextView.setRotation(roll);
+            //mTextView.setRotationX(pitch);
+            //mTextView.setRotationY(yaw);
+        }
+
+        // onPose() is called whenever a Myo provides a new pose.
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            // Handle the cases of the Pose enumeration, and change the text of the text view
+            // based on the pose we receive.
+            switch (pose) {
+                case UNKNOWN:
+                    System.out.println("unknown");
+                    break;
+                case REST:
+                case DOUBLE_TAP:
+                    System.out.println("double tap");
+                    break;
+                case FIST:
+                    System.out.println("fist");
+                    break;
+                case WAVE_IN:
+                    System.out.println("wave in");
+                    break;
+                case WAVE_OUT:
+                    System.out.println("wave out");
+                    break;
+                case FINGERS_SPREAD:
+                    System.out.println("spread");
+                    break;
+            }
+
+            if (pose != Pose.UNKNOWN && pose != Pose.REST) {
+                // Tell the Myo to stay unlocked until told otherwise. We do that here so you can
+                // hold the poses without the Myo becoming locked.
+                myo.unlock(Myo.UnlockType.HOLD);
+
+                // Notify the Myo that the pose has resulted in an action, in this case changing
+                // the text on the screen. The Myo will vibrate.
+                myo.notifyUserAction();
+            } else {
+                // Tell the Myo to stay unlocked only for a short period. This allows the Myo to
+                // stay unlocked while poses are being performed, but lock after inactivity.
+                myo.unlock(Myo.UnlockType.TIMED);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //initialize the hub to be used by the myo device
+        // First, we initialize the Hub singleton with an application identifier.
         Hub hub = Hub.getInstance();
-        if (!hub.init(this)) {
-            Log.e(TAG, "Could not initialize the Hub.");
+        if (!hub.init(this, getPackageName())) {
+            // We can't do anything with the Myo device if the Hub can't be initialized, so exit.
+            Toast.makeText(this, "Couldn't initialize Hub", Toast.LENGTH_SHORT).show();
             finish();
             return;
         }
+        // Next, register for DeviceListener callbacks.
+        hub.addListener(mListener);
+        YourActivity = this;
+        if (ContextCompat.checkSelfPermission(YourActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(YourActivity,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            askForLocationPermissions();
+        }
 
-        // Will connect with a myo device that is very close to phone
-        Hub.getInstance().attachToAdjacentMyo();
+        onScanActionSelected();
+
+        System.out.println("done");
     }
 
     private Button b;
@@ -54,5 +186,73 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    private void onScanActionSelected() {
+        // Launch the ScanActivity to scan for Myos to connect to.
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
+    }
+
+    private void askForLocationPermissions() {
+
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            new android.support.v7.app.AlertDialog.Builder(this)
+                    .setTitle("Location permessions needed")
+                    .setMessage("you need to allow this permission!")
+                    .setPositiveButton("Sure", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(YourActivity,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    LOCATION_PERMISSION_REQUEST_CODE);
+                        }
+                    })
+                    .setNegativeButton("Not now", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+//                                        //Do nothing
+                        }
+                    })
+                    .show();
+
+            // Show an expanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+
+        } else {
+
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+
+            // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if (isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    //Do you work
+                } else {
+                    Toast.makeText(this, "Can not proceed! i need permission" , Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+    public static boolean isPermissionGranted(@NonNull String[] grantPermissions, @NonNull int[] grantResults,
+                                              @NonNull String permission) {
+        for (int i = 0; i < grantPermissions.length; i++) {
+            if (permission.equals(grantPermissions[i])) {
+                return grantResults[i] == PackageManager.PERMISSION_GRANTED;
+            }
+        }
+        return false;
     }
 }
