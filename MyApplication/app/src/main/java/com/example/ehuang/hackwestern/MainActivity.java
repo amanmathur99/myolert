4b7dcb179de185eb783c77a155eda9e29e3dd2dd
eyhhuang <emily.huang1@hotmail.com>
2017-11-18 05:35:03 -0500

    added call function, probably

--- MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ehuang/hackwestern/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.ehuang.hackwestern;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private Button b;
+    String num = "1234567890";
+    public void addListenerOnButtonClick(){
+        b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        Intent callintent = new Intent(Intent.ACTION_CALL);
+                        callintent.setData(Uri.parse("tel:"+num));
+                        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                            return;
+                        }
+                        startActivity(callintent);
+                    }
+                }
+        );
     }
 }
